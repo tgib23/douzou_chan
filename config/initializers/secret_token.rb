2c194bf8171b995963347da363fa089981a23260
satoshi yamada <satoshi@kikugoro.local>
2013-08-18 11:04:32 +0900

    bundle update and change token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DouzouApp::Application.config.secret_key_base = '04bb86cd93f89c833e2313ca5b7b69ced6199e21f6bcb950b048e9b34c7931b1476bd77bf7b9fa68223ce27457ea1688f3d890247e337a3acc299cdf2849c168'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
