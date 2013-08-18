d8c23731f0c639e27a7bc9f0f8f20d0a10a13a37
satoshi yamada <satoshi@kikugoro.local>
2013-08-18 10:44:27 +0900

    start douzou_chan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DouzouApp::Application.config.secret_key_base = '04bb86cd93f89c833e2313ca5b7b69ced6199e21f6bcb950b048e9b34c7931b1476bd77bf7b9fa68223ce27457ea1688f3d890247e337a3acc299cdf2849c168'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DouzouApp::Application.config.session_store :cookie_store, key: '_douzou_app_session'
